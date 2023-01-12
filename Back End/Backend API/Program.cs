ef24b3c00e132f3d9d6ae9f5e4a298b8b8816021
sergio185678 <flores.ofiuco.sergio@gmail.com>
2023-01-12 08:56:17 -0500

    quinto avance
    acomode mejor el backend y agrege algunas cosas

---
+++ Back End/Backend API/Program.cs
@@ -0,0 +1,32 @@
+using Demo.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();//los controladores deben cargar primero
+//////////////Activo la conexion con la base de datos
+builder.Services.AddDbContext<DemoDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+/////////////////////
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
