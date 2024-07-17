5cd0fdba4432540b6b899a4b627b13106227461a
Sergio Flores <67492035+sergio185678@users.noreply.github.com>
2024-07-17 17:50:08 -0500

    Update add-token.interceptor.ts

--- FrontEnd/src/app/helpers/add-token.interceptor.ts
+++ FrontEnd/src/app/helpers/add-token.interceptor.ts
@@ -10,11 +10,12 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { LoginService } from '../services/login.service';
 
 @Injectable()
 export class AddTokenInterceptor implements HttpInterceptor {
 
-  constructor(private router:Router,private toastr:ToastrService) {}
+  constructor(private router:Router,private toastr:ToastrService,private loginService:LoginService,) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token=localStorage.getItem('token');
@@ -26,6 +27,7 @@ export class AddTokenInterceptor implements HttpInterceptor {
       catchError((error: HttpErrorResponse) => {
           if (error && (error.status == 401||error.status == 0) ){//porsicaso pongo tambien en status 0
             this.toastr.error('Sesion expirada','Error!');
+            this.loginService.removelocalStorage();
             this.router.navigate(['/inicio/login']);
           }
           return throwError(error);
